e1808fec615eda72a8b0afd9f3cc811b1d95d484
minimous <khanhhoai95@gmail.com>
2017-04-26 10:35:14 +0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  #post '/assignment', to: 'assignments#create'
+  post '/get_assignments', to: 'assignments#get_assignments'
   post '/get_groups', to: 'groups#get_groups'
   resources :users, only: [:show, :update]
   resources :assignments, only: [:create, :destroy]

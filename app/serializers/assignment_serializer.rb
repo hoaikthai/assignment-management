757acbb41b22fb84d1511e821165401a4cce01fb
minimous <khanhhoai95@gmail.com>
2017-04-28 12:06:14 +0700

    Edit time format for duedate

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,3 +1,7 @@
 class AssignmentSerializer < BaseSerializer
   attributes :id, :subject, :content, :due_date, :attachment, :group_id
+
+  def due_date
+  	object.due_date.strftime("%d %b %Y - %H:%M") if object.due_date
+  end
 end

46c7abcccc28a4f19347da54394844243b9c959f
minimous <khanhhoai95@gmail.com>
2017-04-29 16:44:49 +0700

    Fix get submission

--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -2,6 +2,6 @@ class SubmissionSerializer < BaseSerializer
   attributes :id, :user_id, :assignment_id, :sub_date, :attachment, :score, :group_id
 
   def sub_date
-  	object.due_date.strftime("%d %b %Y - %H:%M") if object.due_date
+  	object.sub_date.strftime("%d %b %Y - %H:%M") if object.sub_date
   end
 end

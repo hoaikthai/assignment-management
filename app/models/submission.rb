a5ad0a6bbfd7aae162185b4b3d4370dc4a1fb773
minimous <khanhhoai95@gmail.com>
2017-04-29 16:54:27 +0700

    Edit submission json

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,6 @@
 class Submission < ApplicationRecord
   belongs_to :user
   belongs_to :assignment
+  belongs_to :group
   validates :attachment, presence: true
 end
--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,7 +1,11 @@
 class SubmissionSerializer < BaseSerializer
-  attributes :id, :user_id, :assignment_id, :sub_date, :attachment, :score, :group_id
+  attributes :id, :user, :assignment_id, :sub_date, :attachment, :score, :group_id
 
   def sub_date
   	object.sub_date.strftime("%d %b %Y - %H:%M") if object.sub_date
+  end
+
+  def user
+  	object.user.name
   end
 end

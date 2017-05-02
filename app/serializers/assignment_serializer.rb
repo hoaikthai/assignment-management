cd26e36f6ea9bfa4027db9c91addb16613a25fa6
minimous <khanhhoai95@gmail.com>
2017-05-02 18:14:30 +0700

    Fix assignment serializer

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,5 +1,5 @@
 class AssignmentSerializer < BaseSerializer
-  attributes :id, :subject, :content, :due_date, :attachment, :group_id, :user_id
+  attributes :id, :subject, :content, :due_date, :attachment, :group_id, :user_id, :file_format
 
   def due_date
   	object.due_date.strftime("%d %b %Y - %H:%M") if object.due_date
